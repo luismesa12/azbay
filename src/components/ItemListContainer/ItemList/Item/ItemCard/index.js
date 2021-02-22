6a949bcd2ffa1ceb2ff5726cfa5a2881498705f3
luismesa12 <luismesa12@hotmail.com>
2021-02-22 13:51:50 -0500

    Estado cantidad seleccionada

--- src/components/ItemListContainer/ItemList/Item/ItemCard/index.js
+++ src/components/ItemListContainer/ItemList/Item/ItemCard/index.js
@@ -4,16 +4,21 @@ import ItemCount from "../../../ItemCount"
 
 const ItemCard = ({ product }) => {
     const [goToCart, setGoToCart] = useState(false);
+    const [q, setQ] = useState({});
     const onAdd = (count)=>{
-        setGoToCart(true)
+        setQ({quantity: count, item: product});
+        setGoToCart(true);
         console.log(count);
     };
+    console.log(q);
     return (
         <div className="card">
             <div className="card-body">
+                <Link to={`/item/${product.id}`}>
                 <img src={product.pictureUrl} />
                 <h4 className="card-title">{product.title}</h4>
                 <h5>{(product.price)?`$${product.price}`:""}</h5>
+                </Link>
                 {goToCart?<Link to='/carrito'><button>Ir al Carrito</button></Link>:<ItemCount stock={5} initial={1} onAdd={onAdd}></ItemCount>}
             </div>
         </div>
