0cff4874eb0759a78bce843f7b65092b7593d35d
luismesa12 <luismesa12@hotmail.com>
2021-02-27 11:55:55 -0500

    Ajuste tempCart (no modificar estado directamente)

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -12,8 +12,9 @@ export const CartProvider = ({ children }) => {
     function addToCart(item) {
         if (cart.length !== 0) {
             if (indexInCart(item) >= 0) {
-                cart[indexInCart(item)].quantity += item.quantity;
-                setCart(cart)
+                let tempCart = [...cart]
+                tempCart[indexInCart(item)].quantity += item.quantity;
+                setCart(tempCart)
             }
             else {
                 setCart([...cart, item])
