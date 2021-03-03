066ba8735a4d0524da3f328eee944af458c06759
luismesa12 <luismesa12@hotmail.com>
2021-03-03 08:20:58 -0500

    Total a pagar

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -6,12 +6,29 @@ import CartItem from "./CartItem";
 
 const Cart = () => {
     const { cart } = useCartContext();
+    const tempCart = [...cart];
+    const accum = tempCart.reduce(totalQ, 0)
+
+    function totalQ(acc, i) {
+        return acc += i.price * i.quantity
+    }
     return (
         <>
             <h1>Soy El Carrito</h1>
             {
                 cart.length ?
-                    cart.map((item) => <span key={item.id}> <CartItem item={item} /> </span>)
+                    <>
+                        {cart.map((item) => <span key={item.id}> <CartItem item={item} /> </span>)}
+                        <div className='container'>
+                            <div className='card'>
+                                <div className='card-body'>
+                                    <h3>
+                                        Total a pagar: ${accum}
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                    </>
                     : <h1>Carrito Vacio <Link to='/'><button>Ir a Comprar</button></Link></h1>
             }
 
--- src/components/CartWidget/index.js
+++ src/components/CartWidget/index.js
@@ -1,19 +1,19 @@
 
 import { useCartContext } from "../../context/CartContext";
 
-const CartWidget = ()=>{
+const CartWidget = () => {
     const { cart } = useCartContext();
-   
+
     const tempCart = [...cart];
-    const accum = tempCart.reduce(totalQ,0)
-    
+    const accum = tempCart.reduce(totalQ, 0)
+
     function totalQ(acc, i) {
         return acc += i.quantity
     }
-    return(
+    return (
         <>
-        <img src="../../img/carrito-de-compras.svg" width="40" alt="carrito"/>
-        <span class="badge badge-light">{accum||null}</span>
+            <img src="../../img/carrito-de-compras.svg" width="40" alt="carrito" />
+            <span class="badge badge-light">{accum || null}</span>
         </>
     )
 }
