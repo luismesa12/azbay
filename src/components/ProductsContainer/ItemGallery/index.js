46fc71b20605fbe8a1a3d650997a744a1408c4bc
luismesa12 <luismesa12@hotmail.com>
2021-03-21 16:20:55 -0500

    Item Gallery con material ui

--- src/components/ProductsContainer/ItemGallery/index.js
+++ src/components/ProductsContainer/ItemGallery/index.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import ShowMore from "../ShowMore";
 import ItemCard from "./ItemCard";
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box';
 
 const ItemGallery = ({ productsState }) => {
     const [productsSlice, setproductsSlice] = useState([])
@@ -8,14 +10,22 @@ const ItemGallery = ({ productsState }) => {
         setproductsSlice(e)
     };
     return (
-        <>
-            <div className='card-columns'>
-                {
-                    productsSlice.map((product) => <span key={product.id}><ItemCard product={product} /></span>)
-                }
-            </div>
-            <ShowMore {...{ productsState, handleProductsSlice }} productsPerPag={6} />
-        </>
+            <Box pt={5}>
+                <Grid container spacing={4}>
+
+                    {
+                        productsSlice.map((product) =>
+                            <Grid key={product.id} item xs={12} sm={6} md={4}>
+                                <ItemCard product={product} />
+                            </Grid>
+                        )
+                    }
+
+                </Grid>
+                <Box p={5}>
+                    <ShowMore {...{ productsState, handleProductsSlice }} productsPerPag={6} />
+                </Box>
+            </Box>
     )
 };
 export default ItemGallery;
