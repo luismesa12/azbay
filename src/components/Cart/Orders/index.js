d01d54a1694b08a13d4152cb3dd13e03f63f5299
luismesa12 <luismesa12@hotmail.com>
2021-03-23 02:10:13 -0500

    validacion form order

--- src/components/Cart/Orders/index.js
+++ src/components/Cart/Orders/index.js
@@ -11,13 +11,45 @@ const Orders = () => {
     const [currentOrder, setCurrentOrder] = useState({});
     const [orderOk, setOrderOk] = useState(false);
     const [orderError, setOrderError] = useState(false);
-
+    const [validated, setValidated] = useState({})
     const tempCart = [...cart];
     const accum = tempCart.reduce(totalQ, 0);
 
     function totalQ(acc, i) {
         return acc += i.price * i.quantity
     };
+    /* Validation.. */
+    const validate=(values)=>{
+        let aux = {};
+        aux.name = values.name.trim().length>1?false:'Nombre es requerido'
+        aux.lastName = values.lastName.trim().length>1?false:'Apellido es requerido'
+        aux.number = values.number.length>9?false:'Minimo 10 caracteres requeridos'
+        aux.email = (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/).test(values.email)?false:'Email no valido'
+        console.log(aux);
+        setValidated(aux)
+        return aux
+    };
+    const resetValidateName = ()=>{
+        const aux = {...validated}
+        aux.name=false
+        setValidated(aux)
+    }
+    const resetValidateLName = ()=>{
+        const aux = {...validated}
+        aux.lastName=false
+        setValidated(aux)
+    }
+    const resetValidateEmail = ()=>{
+        const aux = {...validated}
+        aux.email=false
+        setValidated(aux)
+    }
+    const resetValidateNumber = ()=>{
+        const aux = {...validated}
+        aux.number=false
+        setValidated(aux)
+    }
+    /*..Validation*/
     /* */
     async function createOrder(e) {
         e.preventDefault();
@@ -27,28 +59,36 @@ const Orders = () => {
             number: document.getElementById("pnumber").value,
             email: document.getElementById("email").value
         };
-        const newOrder = {
-            ...buyer,
-            items: cart,
-            date: firebase.firestore.FieldValue.serverTimestamp(),
-            total: accum
-        };
-
-        const orders = db.collection("orders");
-
-        try {
-            await orders.add(newOrder).then(id => {
-                console.log('Order created with id: ', id.id);
-                setCurrentOrder({ id: id.id, ...newOrder });
-                clearCart();
-                e.target.reset();
-                setOrderOk(true)
-            });
+        const tempVal = validate(buyer);
+        if (tempVal.name||tempVal.lastName ||tempVal.email ||tempVal.number ) {
+            console.log("hay error");
         }
-        catch (err) {
-            console.error(err);
-            setOrderError(true)
+        else{
+            console.log('no hay erreor');
+            const newOrder = {
+                ...buyer,
+                items: cart,
+                date: firebase.firestore.FieldValue.serverTimestamp(),
+                total: accum
+            };
+    
+            const orders = db.collection("orders");
+    
+            try {
+                await orders.add(newOrder).then(id => {
+                    console.log('Order created with id: ', id.id);
+                    setCurrentOrder({ id: id.id, ...newOrder });
+                    clearCart();
+                    e.target.reset();
+                    setOrderOk(true)
+                });
+            }
+            catch (err) {
+                console.error(err);
+                setOrderError(true)
+            }
         }
+        
     }
     /*  */
 
@@ -94,12 +134,22 @@ const Orders = () => {
                                             <TextField
                                                 id="fname"
                                                 label="Nombre"
+                                                required
+                                                helperText={validated.name}
+                                                error={Boolean(validated.name)}
+                                                onChange={resetValidateName}
+                                                disabled={!Boolean(accum)}
                                             />
                                         </Grid>
                                         <Grid item xs={12}>
                                             <TextField
                                                 id="sname"
                                                 label="Apellido"
+                                                required
+                                                helperText={validated.lastName}
+                                                error={Boolean(validated.lastName)}
+                                                onChange={resetValidateLName}
+                                                disabled={!Boolean(accum)}
                                             />
                                         </Grid>
                                         <Grid item xs={12}>
@@ -107,6 +157,11 @@ const Orders = () => {
                                             <TextField
                                                 id="pnumber"
                                                 label="Celular"
+                                                required
+                                                helperText={validated.number}
+                                                error={Boolean(validated.number)}
+                                                onChange={resetValidateNumber}
+                                                disabled={!Boolean(accum)}
                                             />
                                         </Grid>
                                         <Grid item xs={12}>
@@ -114,6 +169,11 @@ const Orders = () => {
                                             <TextField
                                                 id="email"
                                                 label="Email"
+                                                required
+                                                helperText={validated.email}
+                                                error={Boolean(validated.email)}
+                                                onChange={resetValidateEmail}
+                                                disabled={!Boolean(accum)}
                                             />
                                         </Grid>
                                     </Grid>
